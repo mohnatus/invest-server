b031ea9708a8ad9050cca3945dc31759e31c3764
FurryCat <hyp-hyper.cat@yandex.ru>
2021-04-23 23:01:20 +0300

    etfs list

--- routes/composition.js
+++ routes/composition.js
@@ -1,16 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-const composition = require('../data/composition');
+var fileName = 'composition.json';
+var fs = require('fs');
+
+let composition = {};
+
+function updateComposition(data) {
+  composition = data;
+  saveComposition();
+}
+
+function saveComposition() {
+  return new Promise((res, rej) => {
+    let data = JSON.stringify(composition);
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    });
+  });
+}
+
+fs.access(fileName, (err) => {
+  if (err) {
+    saveComposition()
+      .then(() => console.log('Файл composition.json создан'))
+      .catch((err) => console.error(err));
+  } else {
+    let str = fs.readFileSync(fileName);
+    composition = JSON.parse(str);
+    console.log('Получен состав портфеля:', composition);
+  }
+});
 
 router.get('/', function (req, res, next) {
-  //res.set('Access-Control-Allow-Origin', '*');
-  return res.json(composition);
+  return res.json({ composition });
+});
+
+router.get('/percents', function (req, res, next) {
+  return res.json({ percents: composition.percents });
 });
 
 router.post('/', function (req, res, next) {
-  //res.set('Access-Control-Allow-Origin', '*');
-  return res.json(composition);
+  return res.json(updateComposition(req.body));
 });
 
 module.exports = router;
--- routes/etfs.js
+++ routes/etfs.js
@@ -6,45 +6,58 @@ var fileName = 'etfs.json';
 var fs = require('fs');
 
 let etfs = [];
+let counter = 1;
 
-function updateEtf(data) {
+function updateEtf(id, data) {
+  if (!id) {
+    return {
+      error: 'No id'
+    };
+  }
+  console.log('update 2', id);
   if (!data.ticker) {
     return {
       error: 'No ticker'
     };
-  } else {
-    let index = etfs.findIndex((e) => e.ticker == data.ticker);
-    if (index > -1) {
-      etfs.splice(index, 1, data);
-    } else {
-      etfs.push(data);
-    }
-
-    saveEtfs();
-
-    return {
-      etfs
-    };
   }
+
+  let etf = {
+    ...data,
+    id: id,
+  };
+
+  let index = etfs.findIndex((e) => e.id == id);
+
+
+  if (index > -1) {
+    etfs.splice(index, 1, etf);
+  } else {
+    etfs.push(etf);
+  }
+
+  saveEtfs();
+
+  return {
+    etfs
+  };
 }
 
-function deleteEtf(ticker) {
-  if (!ticker) {
+function deleteEtf(id) {
+  if (!id) {
     return {
-      error: 'No ticker'
-    };
-  } else {
-    let index = etfs.findIndex((e) => e.ticker == ticker);
-    if (index > -1) {
-      etfs.splice(index, 1);
-    }
-
-    saveEtfs();
-
-    return {
-      etfs
+      error: 'No id'
     };
   }
+  let index = etfs.findIndex((e) => e.id == id);
+  if (index > -1) {
+    etfs.splice(index, 1);
+  }
+
+  saveEtfs();
+
+  return {
+    etfs
+  };
 }
 
 function saveEtfs() {
@@ -62,11 +75,14 @@ function saveEtfs() {
 
 fs.access(fileName, (err) => {
   if (err) {
-    saveEtfs().then(() => console.log('Файл etfs.json создан')).catch(err => console.error(err));
+    saveEtfs()
+      .then(() => console.log('Файл etfs.json создан'))
+      .catch((err) => console.error(err));
   } else {
     let str = fs.readFileSync(fileName);
     etfs = JSON.parse(str);
-    console.log('Получены фонды:', etfs);
+    counter = Math.max(...etfs.map((e) => e.id)) + 1;
+    console.log('Получены фонды:', etfs, counter);
   }
 });
 
@@ -78,15 +94,15 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/create', jsonParser, function (req, res, next) {
-  res.json(updateEtf(req.body));
+  res.json(updateEtf(counter++, req.body));
 });
 
-router.post('/update', function (req, res, next) {
-  res.json(updateEtf(req.body));
+router.post('/update/:id', function (req, res, next) {
+  res.json(updateEtf(req.params['id'], req.body));
 });
 
-router.get('/delete/:ticker', function (req, res, next) {
-  res.json(deleteEtf(req.params['ticker']));
+router.get('/delete/:id', function (req, res, next) {
+  res.json(deleteEtf(req.params['id']));
 });
 
 module.exports = router;
